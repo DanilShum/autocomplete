677f77283a7355163000af0f75a7de379a67129d
Andrey Klimash <a.klimash@icloud.com>
2020-04-18 21:44:03 +0300

    Revert "Revert "first task""

---
+++ autocmplete.js
@@ -0,0 +1,26 @@
+let search__label = document.querySelector('.search__label');
+let search__input = document.querySelector('.search__input');
+let search__section = document.querySelector('.search__section');
+
+let search__arrow = document.querySelector('.search__arrow');
+let search__arrowWrapper = document.querySelector('.search__arrow-wrapper');
+
+
+
+search__section.onclick = function() {
+    if (search__section.click) {
+        search__input.focus();
+        search__arrowWrapper.classList.add('search__arrow-wrapper__transform');
+        search__label.classList.add('search__label-transition');
+    } 
+};
+search__input.onblur = function() {
+    if (search__input.onblur) {
+        search__label.classList.remove('search__label-transition');
+        search__arrowWrapper.classList.remove('search__arrow-wrapper__transform');
+    }
+};
+
+
+
+
