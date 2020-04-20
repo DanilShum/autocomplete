af0757bdb9ccd32501e676effadbe9f0819ffb57
DanilShum <danil.shumilkin.96@gmail.com>
2020-04-20 09:46:22 +0300

    open and drop to listing

--- autocmplete.js
+++ autocmplete.js
@@ -5,27 +5,22 @@ let search__section = document.querySelector('.search__section');
 let search__arrow = document.querySelector('.search__arrow');
 let search__arrowWrapper = document.querySelector('.search__arrow-wrapper');
 
+let search__listingWrapper = document.querySelector('.search__listing-wrapper');
+let search__listing = document.querySelector('.search__listing');
+let search__li = search__listing.getElementsByTagName('li');
 
 
-search__section.onclick = function() {
-    if (search__section.click) {
-        search__input.focus();
-        search__arrowWrapper.classList.add('search__arrow-wrapper__transform');
-        search__label.classList.add('search__label-transition');
-    } 
+
+for(i=0;i<search__li.length;i++) {
+    search__li[i].onclick = function(evt) {
+        evt.preventDefault();
+        if (this) {
+            search__input.value = this.textContent;
+        }
+    };
 };
-search__input.onfocus = function() {
-    if (search__input.onfocus) {
-        search__label.classList.add('search__label-transition');
-        search__arrowWrapper.classList.add('search__arrow-wrapper__transform');
-    }
-};
-search__input.onblur = function() {
-    if (search__input.onblur) {
-        search__label.classList.remove('search__label-transition');
-        search__arrowWrapper.classList.remove('search__arrow-wrapper__transform');
-    }
-};
+
+
 
 
 
