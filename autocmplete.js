35d2fb8c31ae9065bb1ef56866fd70bbe81c4e9a
DanilShum <danil.shumilkin.96@gmail.com>
2020-04-22 12:14:50 +0300

    Search v.3

--- autocmplete.js
+++ autocmplete.js
@@ -1,44 +1,39 @@
-let search__label = document.querySelector('.search__label');
-let search__input = document.querySelector('.search__input');
-let search__section = document.querySelector('.search__section');
-
-let search__arrow = document.querySelector('.search__arrow');
-let search__arrowWrapper = document.querySelector('.search__arrow-wrapper');
-
-let search__listingWrapper = document.querySelector('.search__listing-wrapper');
-
-let search__listing = document.querySelector('.search__listing');
-let search__li = document.querySelectorAll('.search__listing > li');
-let search__span = document.querySelectorAll('.search__listing > li > span');
-
-let available = search__listing.querySelector('.available');
-
-for(i=0;i<search__li.length;i++) {
-    search__li[i].onclick = function(evt) {
-        evt.preventDefault();
-            search__input.value = this.textContent;
-    };
+// let searchLabel = document.querySelector('.search__label');
+// let searchSection = document.querySelector('.search__section');
+// let searchArrow = document.querySelector('.search__arrow');
+// let searchArrowWrapper = document.querySelector('.search__arrow-wrapper');
+// let searchListingWrapper = document.querySelector('.search__listing-wrapper');
+const searchInput = document.querySelector('.search__input')
+const searchListing = document.querySelector('.search__listing')
+const searchLi = document.querySelectorAll('.search__listing > li')
+const available = searchListing.querySelector('.available')
+let i
+for (i = 0; i < searchLi.length; i++) {
+  searchLi[i].onclick = function (evt) {
+    evt.preventDefault()
+    searchInput.value = this.textContent
+  }
 };
-search__input.oninput = function() {
-    Search();
+searchInput.oninput = function () {
+  Search()
 }
 
-function Search() {
-    let word = search__input.value.toLowerCase();
-    const re = new RegExp(word, 'gi');
-    for (i = 0; i < search__li.length; i++) {
-        if (re.test(search__li[i].innerText)) {
-            search__li[i].style.display = "";
-            search__li[i].innerHTML = search__li[i].innerText.replace(re, `<span>${word}</span>`); /* выделение искомого слова */
-        } else {
-            search__li[i].style.display = "none"; 
-        };
-        
-       
+function Search () {
+  const word = searchInput.value
+  const re = new RegExp('(' + word + ')', 'gi')
+
+  for (i = 0; i < searchLi.length; i++) {
+    if (re.test(searchLi[i].innerText)) {
+      searchLi[i].style.display = ''
+      searchLi[i].innerHTML = searchLi[i].innerText.replace(re, '<span>$1</span>')
+      /* выделение искомого слова */
+    } else {
+      searchLi[i].style.display = 'none'
     };
-    if (re.test(search__listing.innerText)) {
-        available.style.display = "none";
-        } else {
-            available.style.display = "block"; 
-        };
+  };
+  if (re.test(searchListing.innerText)) {
+    available.style.display = 'none'
+  } else {
+    available.style.display = 'block'
+  };
 };
