cb9055df6436d60205f8e3450f65e9f5e5fbc672
DanilShum <danil.shumilkin.96@gmail.com>
2020-04-22 12:43:15 +0300

    search(Not found)

--- autocmplete.js
+++ autocmplete.js
@@ -5,24 +5,22 @@
 // let searchListingWrapper = document.querySelector('.search__listing-wrapper');
 const searchInput = document.querySelector('.search__input')
 const searchListing = document.querySelector('.search__listing')
-const searchLi = document.querySelectorAll('.search__listing > li')
+const searchLi = document.querySelectorAll('.search__listing > li:not(.available)')
 const available = searchListing.querySelector('.available')
-let i
-for (i = 0; i < searchLi.length; i++) {
+
+for (let i = 0; i < searchLi.length; i++) {
   searchLi[i].onclick = function (evt) {
     evt.preventDefault()
     searchInput.value = this.textContent
   }
 };
-searchInput.oninput = function () {
-  Search()
-}
 
-function Search () {
+
+function search () {
   const word = searchInput.value
   const re = new RegExp('(' + word + ')', 'gi')
 
-  for (i = 0; i < searchLi.length; i++) {
+  for (let i = 0; i < searchLi.length; i++) {
     if (re.test(searchLi[i].innerText)) {
       searchLi[i].style.display = ''
       searchLi[i].innerHTML = searchLi[i].innerText.replace(re, '<span>$1</span>')
@@ -37,3 +35,4 @@ function Search () {
     available.style.display = 'block'
   };
 };
+searchInput.oninput = search;
