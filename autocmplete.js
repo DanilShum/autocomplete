0a965bd6cd28745af96e6dedbda95e1349cab9a8
DanilShum <danil.shumilkin.96@gmail.com>
2020-04-25 13:38:33 +0300

    keyboard ver.4

--- autocmplete.js
+++ autocmplete.js
@@ -3,77 +3,86 @@ const searchListing = document.querySelector('.search__listing')
 const available = searchListing.querySelector('.available')
 const searchLi = Array.from(document.querySelectorAll('.search__listing > li:not(.available)'))
 
+
 searchLi.forEach(function (item, i, arr) {
-  item.addEventListener('click', function (evt){
-      evt.preventDefault()
+  item.addEventListener('click', function (evt) {
+    evt.preventDefault()
     const target = evt.target
     searchInput.value = target.textContent
     arr.forEach(function (led) {
       led.classList.toggle('target', led === target)
     })
-  });
-
-  searchInput.addEventListener('blur', function (input) {
-   item.classList.remove('focused')
-  });
+  })
 })
+searchInput.addEventListener('blur', function (input) {
+    const focusedLi = document.querySelector('.focused');
+    focusedLi.classList.remove('focused');
+});
 searchInput.addEventListener('keydown', function (event) {
-  if (event.code == 'ArrowDown') {
+  if (event.code === 'ArrowDown') {
     event.preventDefault()
     const focusedLi = document.querySelector('.focused')
     if (focusedLi) {
       const nextLi = getNextSibling(focusedLi, ':not(.hidden__li):not(.available)')
+      
       if (nextLi) {
         focusedLi.classList.remove('focused')
         nextLi.classList.add('focused')
-        const heightLi = focusedLi.clientHeight;
-        nextLi.scrollIntoView(heightLi)
-      } else  {
-        searchListing.scrollTo(pageXOffset, 0);
+        focusedLi.scrollIntoView(true)
+      } else {
+        searchListing.scrollTo(0, 0)
         const lastLI = document.querySelectorAll('.search__listing > li:not(.hidden__li):not(.available)')
         focusedLi.classList.remove('focused')
-        lastLI[0].classList.add('focused')// для focusedLi не находит значение, не пойму в чем дело
-       
+        lastLI[0].classList.add('focused')
       }
     } else {
       searchLi[0].classList.add('focused')
+      
     }
-  }
-  if (event.code == 'ArrowUp') {
+  } 
+  if (event.code === 'ArrowUp') {
     event.preventDefault()
     const focusedLi = document.querySelector('.focused')
     if (focusedLi) {
       const prevLi = getPreviousSibling(focusedLi, ':not(.hidden__li):not(.available)')
-       if (prevLi) {
+      
+      if (prevLi) {
         focusedLi.classList.remove('focused')
         prevLi.classList.add('focused')
-        const heightLi = focusedLi.clientHeight;
-        prevLi.scrollIntoView(heightLi)
-      }
-      else  {
+        prevLi.scrollIntoView(true)
+      } else {
         const lastLI = document.querySelectorAll('.search__listing > li:not(.hidden__li):not(.available)')
         lastLI[lastLI.length - 1].classList.add('focused')
         focusedLi.classList.remove('focused')
         searchListing.scrollTop = searchListing.scrollHeight
+        
       }
     } else {
       searchLi[0].classList.add('focused')
+      
     }
-  }
-  if (event.code == 'Enter') {
+  } 
+  
+  if (event.code === 'Enter') {
     event.preventDefault()
     const focusedLi = document.querySelector('.focused')
     searchInput.value = focusedLi.textContent
-    focusedLi.classList.toggle('target')
+    searchLi.forEach(function (item, i, arr) {
+      arr.forEach(function (led) {
+        led.classList.toggle('target', led === focusedLi)
+      })
+    })
     searchInput.blur()
-  } else {
-    const focusedLi = document.querySelector('.focused')
-    focusedLi.classList.remove('target')}
-  if (event.code == 'Escape') {
+  }
+
+  if (event.code === 'Escape') {
     event.preventDefault()
     searchInput.blur()
   }
 })
+
+
+
 const getNextSibling = function (elem, selector) {
   // Get the next sibling element
   let sibling = elem.nextElementSibling
@@ -102,30 +111,24 @@ const getPreviousSibling = function (elem, selector) {
     sibling = sibling.previousElementSibling
   }
 }
-// вот почему гит необхоим)я погнал
 function search () {
   const word = searchInput.value
   const re = new RegExp('(' + word + ')', 'gi')
 
   searchLi.forEach(function (item, i, arr) {
     if (re.test(item.innerText)) {
-
       item.classList.remove('hidden__li')
       item.innerHTML = item.innerText.replace(re, '<span>$1</span>')
       /* выделение искомого слова */
     } else {
       item.classList.add('hidden__li')
-
     };
   })
-
   if (re.test(searchListing.innerText)) {
     available.style.display = 'none'
   } else {
     available.style.display = 'block'
   };
 };
-
-
 
 searchInput.oninput = search
